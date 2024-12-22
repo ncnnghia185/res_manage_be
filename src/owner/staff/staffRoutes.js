a256aab6aceebb24543c6741dc7d82fac688b659
ncnnghia185 <ncn180501@gmail.com>
2024-12-22 16:53:23 +0700

    [UPDATE] staff apis

--- src/owner/staff/staffRoutes.js
+++ src/owner/staff/staffRoutes.js
@@ -2,11 +2,24 @@ const router = require("express").Router();
 const staffControllers = require("./staffControllers");
 const timesheetPaymentControllers = require("./timesheet_payroll/timesheetPayrollControllers");
 const monthSalaryControllers = require("./month salary/monthSalaryController");
-router.post("/add-staff", staffControllers.createNewStaff);
-router.get("/all-staffs", staffControllers.getAllStaffsInfor);
-router.get("/infor/:id", staffControllers.getOneStaffInfor);
-router.put("/update/:id", staffControllers.updateOneStaffInfor);
-router.delete("/delete/:id", staffControllers.delteOneStaffInfor);
+const { verifyAccessToken } = require("../../../middlewares/verifyToken");
+router.post("/add-staff", verifyAccessToken, staffControllers.createNewStaff);
+router.get(
+  "/all-staffs",
+  verifyAccessToken,
+  staffControllers.getAllStaffsInfor
+);
+router.get("/infor/:id", verifyAccessToken, staffControllers.getOneStaffInfor);
+router.put(
+  "/update/:id",
+  verifyAccessToken,
+  staffControllers.updateOneStaffInfor
+);
+router.delete(
+  "/delete/:id",
+  verifyAccessToken,
+  staffControllers.delteOneStaffInfor
+);
 
 // Timesheet and Payroll for specific staff
 router.post("/daily-work/:sId", timesheetPaymentControllers.createNewDailyWork);
--- src/owner/staff/staffServices.js
+++ src/owner/staff/staffServices.js
@@ -8,51 +8,61 @@ const insertNewStaff = async (data) => {
   const value = validateStaff(data);
 
   const result = await client.query(
-    `INSERT INTO staff(fullname, gender, date_of_birth, phonenumber, address, identification_card, hire_date, restaurant_id, net_salary, status_work)
-  	VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING *`,
+    `INSERT INTO staff_infor (
+    id, fullname, gender, date_of_birth, phone_number, address, identification_card, hire_date, net_salary, staff_type, position, owner_id, restaurant_id
+    )
+  	VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13) RETURNING *`,
     [
+      value.id,
       value.fullname,
       value.gender,
       value.date_of_birth || null,
-      value.phonenumber,
+      value.phone_number,
       value.address || null,
       value.identification_card || null,
       value.hire_date,
-      value.restaurant_id,
       value.net_salary,
-      value.status_work,
+      value.staff_type,
+      value.position,
+      value.owner_id,
+      value.restaurant_id,
     ]
   );
   return result.rows[0];
 };
 
 // SELECT ONE STAFF
-const selectOneStaff = async (id) => {
-  const condition = parseInt(id);
-  const result = await client.query(`SELECT * FROM staff WHERE id = $1`, [
-    condition,
-  ]);
+const selectOneStaff = async (id, oId, rId) => {
+  const result = await client.query(
+    `SELECT * FROM staff_infor WHERE id = $1 AND owner_id = $2 AND restaurant_id = $3`,
+    [id, oId, rId]
+  );
   return result.rows[0];
 };
 
 // SELECT ALL STAFFS
-const selectAllStaffs = async () => {
-  const result = await client.query(`SELECT * FROM staff ORDER BY id ASC`);
+const selectAllStaffs = async (oId, rId) => {
+  const result = await client.query(
+    `SELECT id, fullname, gender, phone_number,hire_date, net_salary, position, staff_type, status_work FROM staff_infor WHERE owner_id = $1 AND restaurant_id = $2 ORDER BY hire_date ASC`,
+    [oId, rId]
+  );
   return result.rows;
 };
 
 // UPDATE ONE STAFF
-const updateOneStaff = async (id, data) => {
-  const baseQuery = `UPDATE staff SET `;
-  const sqlQuery = updateQuery(baseQuery, id, data);
+const updateOneStaff = async (id, data, oId, rId) => {
+  const baseQuery = `UPDATE staff_infor SET `;
+  const sqlQuery = updateQuery(baseQuery, id, data, oId, rId);
   const result = await client.query(sqlQuery.query, sqlQuery.values);
   return result.rows[0];
 };
 
 // DELETE ONE STAFF
-const deleteOneStaff = async (id) => {
-  const condition = parseInt(id);
-  await client.query(`DELETE FROM staff WHERE id = $1`, [condition]);
+const deleteOneStaff = async (id, oId, rId) => {
+  await client.query(
+    `DELETE FROM staff_infor WHERE id = $1 AND owner_id = $2 AND restaurant_id = $3`,
+    [id, oId, rId]
+  );
 };
 module.exports = {
   insertNewStaff,
